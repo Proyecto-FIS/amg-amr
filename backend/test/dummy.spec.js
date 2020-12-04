b5a964672a5431e2c1bb659a883ea3cc52181c8d
AMG <andresmargar98@gmail.com>
2020-12-04 21:54:05 +0100

    [chore] Added test failure simulation

--- backend/test/dummy.spec.js
+++ backend/test/dummy.spec.js
@@ -3,5 +3,5 @@ require("dotenv").config({ path: __dirname + "/../env/test.env" });
 describe("Test suite", () => {
     test("Test case", () => {
         console.log(process.env.TEST)
-    });
+    });sdfvadfv
 });
