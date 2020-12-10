7df1c2a836d7f617c8d721a27d00ccad0c0f91f2
AMG <andresmargar98@gmail.com>
2020-12-10 13:10:52 +0100

    [test]: Added tests for Validators.Range

--- test/Validators.spec.js
+++ test/Validators.spec.js
@@ -42,4 +42,56 @@ describe("Validators", () => {
         expect(mockedRes.status.mock.calls[0][0]).toBe(400);
         expect(mockedRes.json.mock.calls[0][0]).toMatchObject({ reason: "Missing fields" });
     });
+
+    test("Range below minimum", () => {
+
+        const mockedReq = {
+            body: {
+                myField: 20
+            }
+        };
+
+        const validator = Validators.Range("myField", 21, 30);
+        validator(mockedReq, mockedRes, mockedNext);
+
+        expect(mockedNext.mock.calls.length).toBe(0);
+        expect(mockedRes.status.mock.calls.length).toBe(1);
+        expect(mockedRes.json.mock.calls.length).toBe(1);
+        expect(mockedRes.status.mock.calls[0][0]).toBe(400);
+        expect(mockedRes.json.mock.calls[0][0]).toMatchObject({ reason: "Exceeded boundaries" });
+    });
+
+    test("Range beyond maximum", () => {
+
+        const mockedReq = {
+            body: {
+                myField: 31
+            }
+        };
+
+        const validator = Validators.Range("myField", 21, 30);
+        validator(mockedReq, mockedRes, mockedNext);
+
+        expect(mockedNext.mock.calls.length).toBe(0);
+        expect(mockedRes.status.mock.calls.length).toBe(1);
+        expect(mockedRes.json.mock.calls.length).toBe(1);
+        expect(mockedRes.status.mock.calls[0][0]).toBe(400);
+        expect(mockedRes.json.mock.calls[0][0]).toMatchObject({ reason: "Exceeded boundaries" });
+    });
+
+    test("Range OK", () => {
+
+        const mockedReq = {
+            body: {
+                myField: 25
+            }
+        };
+
+        const validator = Validators.Range("myField", 21, 30);
+        validator(mockedReq, mockedRes, mockedNext);
+
+        expect(mockedNext.mock.calls.length).toBe(1);
+        expect(mockedRes.status.mock.calls.length).toBe(0);
+        expect(mockedRes.json.mock.calls.length).toBe(0);
+    });
 });
